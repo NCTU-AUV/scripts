009405179d9840664c2c5dee05106aed25b8fcd3
CheetosPlumGreenTea <gordondonghsu.ee10@nycu.edu.tw>
2024-07-29 09:10:23 +0100

    Update measure_yaw_drift.py to use Float32MultiArray and improve logging

--- measure_yaw_drift.py
+++ measure_yaw_drift.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float32MultiArray
 
 def callback_function(data):
-    rospy.loginfo("I heard:")
+    rospy.loginfo("I heard: %f", data.data[2])
 
 def listener():
     rospy.init_node('yaw_drift_measurer', anonymous=True)
-	sub = rospy.Subscriber('rpi_to_stm32', Float64MultiArray, callback=callback_function)
-	sub.spin()
+    rospy.Subscriber('rpi_to_stm32', Float32MultiArray, callback=callback_function)
+    rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+	listener()
