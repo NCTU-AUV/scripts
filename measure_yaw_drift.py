62656417ec75bc65908a48a9a740e6d0cdf4159a
DianYueGuo <dianyueguo@gmail.com>
2024-07-29 15:13:20 +0800

    Add script to measure yaw drift using ROS
    - Introduced a new Python script .
    - The script initializes a ROS node named 'listener' and subscribes to the 'rpi_to_stm32' topic.
    - The script logs the third element of the incoming  messages, which is expected to represent yaw drift data.

---
+++ measure_yaw_drift.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+def callback(data):
+    rospy.loginfo("I heard: %f", data[2])
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('rpi_to_stm32', Float64MultiArray, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
